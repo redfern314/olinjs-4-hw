d1899191bbd8514dd76dd5b58ef2ab1d54cd5c04
Derek Redfern <redfern.derek@gmail.com>
2013-02-05 17:35:56 -0500

    Bug fix!

--- routes/users.js
+++ routes/users.js
@@ -8,30 +8,37 @@ var User = mongoose.model('User');
 
 exports.new = function(req, res){
   res.render("newuser",{title:"New User"});
-  console.log(req.session.user);
+  console.log("Current saved id",req.session.user);
 };
 
 exports.new_post = function(req, res){
   console.log(req.body.name);
-  User.find({name: req.body.name}, function(err, docs) {
-        if (err) console.log(err); // ...
+  User.findOne({name: req.body.name}, function(err, doc) {
+        if (err) return console.log(err); // ...
         
-        if (docs.length===0) {
+        if (!doc) {
             var user = new User({name:req.body.name});
-            console.log(user._id);
-            req.session.user = user._id;
+            console.log("User object id",user._id);   
+             
             user.save(function (err){
-                if (err) console.log(err);
+                if (err) return console.log(err);
                 console.log("saved");
-            });
+                req.session.user = user._id;
+            console.log("Object id",user._id);
+            console.log("New saved id",req.session.user); 
           res.send("posted!");
-        } else {
-            console.log(docs[0]._id);
-            console.log("----");
-            console.log(req.session.user);
+            });
+            
 
-            req.session.user = docs[0]._id;
-            console.log(req.session.user);
+        } else {
+            console.log("Current saved id",req.session.user);
+            console.log("Doc id",doc._id);
+            
+
+            req.session.user = doc._id;
+            console.log("New saved id",req.session.user);
+
+            res.send("posted!");
         }
     });
 };
